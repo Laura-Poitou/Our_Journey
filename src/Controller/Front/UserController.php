fd7af6c1590f1856e54cd34f41da936b6a13f4f1
Laura Poitou <poitoulaura@sfr.fr>
2023-04-26 16:01:54 +0200

    Registration and Login Page OK : UserController, JWT token, login form, update nav bar/security.yaml/routes.yaml

---
+++ src/Controller/Front/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class UserController extends AbstractController
+{
+    #[Route('/login', name: 'front_user_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+        
+
+        return $this->render('front/user/login.html.twig', [
+
+        'last_username' => $lastUsername,
+        'error'         => $error,
+        ]);
+    }
+}
