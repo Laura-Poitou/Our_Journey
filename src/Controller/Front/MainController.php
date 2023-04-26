d28043027b00b671d8676e7ecdfa780bf140331a
Laura Poitou <poitoulaura@sfr.fr>
2023-04-26 12:31:11 +0200

    WIP Registration form page: mobile OK

--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -2,17 +2,53 @@
 
 namespace App\Controller\Front;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
+    // Home page
     #[Route('/', name: 'front_main_home')]
-    public function index(): Response
+    public function home(): Response
     {
         return $this->render('front/main/home.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
+
+    // Registration page
+    #[Route('/inscription', name: 'front_main_registration')]
+    public function registration(Request $request, ManagerRegistry $managerRegistry): Response
+    {
+        // user entity to create
+        $user = new User();
+
+        // create form
+        $form = $this->createForm(UserType::class, $user);
+
+        // request treatment by the form
+        $form->handleRequest($request);
+
+        // form validation
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // user entity manager to persist and flush data in data base
+            $em = $managerRegistry->getManager();
+            $em->persist($review);
+            $em->flush();
+
+            // redirection
+            return $this->redirectToRoute('front_main_home');
+            }
+
+        return $this->renderForm('front/main/registration.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
@@ -19,6 +20,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank]
+    #[Assert\Email]
     private ?string $email = null;
 
     #[ORM\Column]
@@ -28,12 +31,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Assert\NotBlank]
     private ?string $password = null;
 
     #[ORM\Column(length: 80)]
+    #[Assert\NotBlank]
+    #[Assert\Type('string')]
+    #[Assert\Length(min: 2, max: 80)]
     private ?string $nickname = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    #[Assert\NotBlank]
+    #[Assert\Date]
     private ?\DateTimeInterface $birthday = null;
 
     #[ORM\Column(length: 255, nullable: true)]
