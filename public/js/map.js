daa6ddd35f0be0348425dba9fa096fcec190e7a0
Laura Poitou <poitoulaura@sfr.fr>
2023-05-12 14:53:51 +0200

    Travel page: interactive map with markers on each visited spot OK

--- public/js/map.js
+++ public/js/map.js
@@ -1,49 +1,36 @@
-// initialize the map 
-// by default Tokyo 
-// search element with id = map
-// setView([latitude, longitude], zoom (between 13 and 16 generally))
-var map = L.map('map').setView([35.681521, 139.756878], 6);
-
-
-// image (of map) from https://leaflet-extras.github.io/leaflet-providers/preview/
-// on stamen.com : JavaScript Libraries paragraph > Leaflet > view demo 
-// Stada OSMBright
-var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-});
-
-// to add images to map
-Stadia_OSMBright.addTo(map);
-
-// add markers and popup (accessility version)
-var markerTokyo = L.marker([35.681521, 139.756878], {alt: 'Tokyo'}).addTo(map).bindPopup('Tokyo');
-var markerNikko = L.marker([36.90125396268982, 139.63572877491868], {alt: 'Nikko'}).addTo(map).bindPopup("<p>Nikko</p>");
-
-
 // to retrieve destinations associative table
 document.addEventListener('DOMContentLoaded', function() {
+	// select div
     var destinationElement = document.querySelector('#js-destinations');
+	// retrieve data in dataset
 	var destinations = destinationElement.dataset.destinations;
-	console.log(destinations);
-
-	// retrieve all latitude 
-
-	// retrieve all longitue 
+	// transform data from JSON to array
+	var destinationsArray = JSON.parse(destinations)
 
 	// retrieve first destination latitude and longitude to initialize the map
+	var firstDestinationLatitude = destinationsArray[0]['latitude']
+	var firstDestinationLongitude = destinationsArray[0]['longitude']
 
 	// initialize the map 
 	// search element with id = map
 	// setView([latitude, longitude], zoom (between 13 and 16 generally))
+	var map = L.map('map').setView([firstDestinationLatitude, firstDestinationLongitude], 6);
 
 	// image (of map) from https://leaflet-extras.github.io/leaflet-providers/preview/
 	// on stamen.com : JavaScript Libraries paragraph > Leaflet > view demo 
 	// Stada OSMBright
+	var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+	});
 
 	// to add images to map
+	Stadia_OSMBright.addTo(map);
 
-	// add markers and popup (accessility version)
+	//markers
+	for (var destination of destinationsArray) {
+		var marker = L.marker([destination.latitude, destination.longitude], {alt: `${destination.name}`}).addTo(map).bindPopup(`${destination.name}`);
+	}
 
 });
 
