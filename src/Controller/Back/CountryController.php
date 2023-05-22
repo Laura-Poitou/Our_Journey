49671d9528c4ae4339c771b16e3a76eb7491e73a
Laura Poitou <poitoulaura@sfr.fr>
2023-05-22 15:50:20 +0200

    WIP rest Countries API: use jwt token in request WIP Service Unavailable

--- src/Controller/Back/CountryController.php
+++ src/Controller/Back/CountryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Back;
 
 use App\Service\restCountriesAPI;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,25 +16,12 @@ class CountryController extends AbstractController
         $this->restCountriesAPI = $restCountriesAPI;
     }
 
-    #[Route('/back/countries', name: 'back_country_browse')]
+    #[Route('/api/countries', name: 'back_country_browse')]
     public function browse(): JsonResponse
     {
-        $countriesInfo = $this->restCountriesAPI->fetchAll();
+        $countriesInfo = new JsonResponse( $this->restCountriesAPI->fetchAll());
 
-        $test = "ma variable transmise";
+        return $countriesInfo;
 
-       return $this->render('front/travel/add.html.twig', [
-        'test' => $test
-       ]);
-
-    }
-
-
-    #[Route('/back/country', name: 'back_country_show')]
-    public function show(): JsonResponse
-    {
-        $countryInfo = $this->restCountriesAPI->fetch('France');
-
-        dd($countryInfo);
     }
 }
--- src/Service/restCountriesAPI.php
+++ src/Service/restCountriesAPI.php
@@ -3,6 +3,8 @@
 namespace App\Service;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 /**
  * Connector to OMDB API
@@ -10,36 +12,45 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class restCountriesAPI
 { 
     private $client;
+    private $jwtManager;
+    private $tokenStorageInterface;
 
-    public function __construct(HttpClientInterface $client)
+    public function __construct(HttpClientInterface $client, TokenStorageInterface $tokenStorageInterface, JWTTokenManagerInterface $jwtManager)
     {
         $this->client = $client;
+        $this->jwtManager = $jwtManager;
+        $this->tokenStorageInterface = $tokenStorageInterface;
     }
+
 
     /**
      * Fetch country information
      * @return array Country data
      */
-    public function fetch(string $name)
+    public function fetch(string $name): array
     {
         // call Rest Countries API for this country name
         $response = $this->client->request(
             // HTTP method
             'GET',
             // API endpoint
-            "https://restcountries.com/v3.1/name/". $name,
+            "https://restcountries.com/v3.1/name/". $name, [
+                'query' => [
+                    'token' => $this->jwtManager->decode($this->tokenStorageInterface->getToken())
+                ]
+            ]
             
         );
 
-        //get status code
+        // gets the HTTP status code of the response
         $statusCode = $response->getStatusCode();
-        // get header
+        // gets the HTTP headers as string[][] with the header names lower-cased
         $contentType = $response->getHeaders()['content-type'][0];
-        // get content
-        $content = $response->getContent();
-        // transform to array
-        $content = $response->toArray();
-        
+        // gets the response body as a string
+        //$content = $response->getContent();
+        // // casts the response JSON content to a PHP array
+         $content = $response->toArray();
+
         return $content;
 
     }
@@ -55,17 +66,14 @@ class restCountriesAPI
             // HTTP method
             'GET',
             // API endpoint
-            "https://restcountries.com/v3.1/all?fields=name,translations",
-            
-        );
+            "https://restcountries.com/v3.1/all?fields=name,translations,flags,latlng", [
+                'query' => [
+                    'token' => $this->jwtManager->decode($this->tokenStorageInterface->getToken())
+                ]
+            ]);
 
-        //get status code
         $statusCode = $response->getStatusCode();
-        // get header
-        $contentType = $response->getHeaders()['content-type'][0];
-        // get content
-        $content = $response->getContent();
-        // transform to array
+        // $contentType = $response->getHeaders()['content-type'][0];
         $content = $response->toArray();
         
         return $content;
