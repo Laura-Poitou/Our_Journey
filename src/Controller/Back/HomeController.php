7712a0467ed28c21fb4531635c4e220dd3d47921
Laura Poitou <poitoulaura@sfr.fr>
2023-05-31 11:17:39 +0200

    WIP HomePage BackOffice and add access control WIP

---
+++ src/Controller/Back/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Back;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/back/home', name: 'back_home_index')]
+    public function index(): Response
+    {
+        return $this->render('back/home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
