9553e3e966c03ae629cf3d5dc02e472d9f15165d
Laura Poitou <poitoulaura@sfr.fr>
2023-05-31 16:44:01 +0200

    WIP Backoffice User page: edit, Back\UserType and _form OK

--- src/Form/Back/UserType.php
+++ src/Form/Back/UserType.php
@@ -3,23 +3,61 @@
 namespace App\Form\Back;
 
 use App\Entity\User;
+use App\Entity\Travel;
+use App\Entity\Traveler;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('nickname')
-            ->add('birthday')
-            ->add('picture')
-            ->add('travelers')
-            ->add('travels')
+            ->add('email', EmailType::class, [
+                'label' => 'Email'
+            ])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Administrateur' => 'ROLE_ADMIN',
+                    'Utilisateur' => 'ROLE_USER', 
+                    'Modérateur' => 'ROLE_MODERATOR'
+                ],
+                'multiple' => true,
+                'expanded' => true
+                ])
+            ->add('nickname', TextType::class, [
+                'label' => 'Pseudo',
+            ])
+            ->add('birthday', BirthdayType::class, [
+                'label' => 'Anniversaire',
+                'placeholder' => [
+                    'year' => 'Année', 'month' => 'Mois', 'day' => 'Jour',
+                ],
+                'format' => 'dd-MM-yyyy',
+            ])
+            ->add('picture', TextType::class, [
+                'label' => 'Image',
+            ])
+            ->add('travelers' , EntityType::class, [
+                'label' => 'Voyageur(s)',
+                'class' => Traveler::class,
+                'choice_label' => 'name',
+                'multiple' => true, 
+                'expanded' => true, 
+            ])
+            ->add('travels', EntityType::class, [
+                'label' => 'Voyage(s)',
+                'class' => Travel::class,
+                'choice_label' => 'id',
+                'multiple' => true, 
+                'expanded' => true, 
+            ])
         ;
     }
 
