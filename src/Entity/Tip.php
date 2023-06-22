81b95f6831c422637d3d65579fe00391feb06374
Laura Poitou <poitoulaura@sfr.fr>
2023-06-22 15:06:24 +0200

    WIP Article Page: isLikedByUser

--- src/Entity/Tip.php
+++ src/Entity/Tip.php
@@ -2,11 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\TipRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\User;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\TipRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: TipRepository::class)]
 class Tip
@@ -147,5 +148,21 @@ class Tip
         }
 
         return $this;
+    }
+
+    /**
+     * To know if this tip is liked by a user
+     *
+     * @param User $user
+     * @return boolean
+     */
+    public function isLikedByUser(User $user) : bool
+    {
+        foreach($this->likes as $like) {
+            if($like->getUser() === $user) {
+                return true;
+            }
+            return false;
+        }
     }
 }
