c135285eb1b9e4eb46c225b216fe6c57ce5c70ee
Laura Poitou <poitoulaura@sfr.fr>
2023-06-22 17:03:01 +0200

    WIP Article Page: Like button on tips OK

--- public/js/like.js
+++ public/js/like.js
@@ -11,9 +11,19 @@ const like = {
         event.preventDefault();
 
         const url = this.href;
+        const spanCount = this.querySelector('span.js-likes');
+        const icone = this.querySelector('i');
 
         axios.get(url).then(function(response) {
-            console.log(response);
+            const likes = response.data.likes;
+            spanCount.textContent = likes;
+
+            if(icone.classList.contains('fa-solid')) {
+                icone.classList.replace('fa-solid', 'fa-regular');
+            } else {
+                icone.classList.replace('fa-regular', 'fa-solid');
+            }
+
         })
         },
 }
